46e05b0adb5488e7c45343c66f8e77c98a3a7c40
Evhen Okhrimenko <oeasoft@gmail.com>
2021-03-05 01:08:38 +0200

    [HW-02] Added an overloaded method with two parameters: a string and the number of repetitions. Updated README.md

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Author information: Okhrimenko Evhen, E-mail:oeasoft@gmail.com
 
 Update history:
 1) Add the main method with output to the console "Hello, world!"
-2) Added to main.java a static method that takes one parameter for output to the console. 
+2) Added to main.java a static method that takes one parameter for output to the console.
+3) Added to main.java an overloaded print method with two parameters: a string,
+   and the number of repetitions of the string output to the console.
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,35 @@ public class Main {
 
         switch (args.length) {
             case 0:
-                System.out.println("Arguments are missing! Enter a string as an argument to output to the console.");
+                System.out.println("Arguments are missing!" + "\n" + "Enter as an argument: only a string or a string and separated by a space " + "\n" + "- the number of times to repeat the output of the line to the console.");
                 break;
             case 1:
                 print(args[0]);
                 break;
             default:
-                String stringToConsole = String.join(" ", args);
-                print(stringToConsole);
+                try {
+                    int lastIndex = (args.length - 1);
+                    StringBuilder string = new StringBuilder();
+                    for (int l = 0; l < lastIndex; l++) {
+                        string.append(args[l]).append(" ");
+                    }
+                    int lastNum = Integer.parseInt(args[lastIndex]);
+                    print(string.toString(), lastNum);
+                } catch (NumberFormatException e) {
+                    String stringToConsole = String.join(" ", args);
+                    print(stringToConsole);
+                }
         }
     }
 
     private static void print(String string) {
         System.out.println(string);
+    }
+
+    private static void print(String string, int quantity) {
+        while (quantity > 0) {
+            System.out.println(string);
+            quantity--;
+        }
     }
 }
